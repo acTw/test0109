979c67bf4babc30c912c746d32dded74e80b82be
Andy Cheng <acheng@cht.com.tw>
2018-01-09 11:36:38 +0800

    get line message

--- receive.php
+++ receive.php
@@ -1,3 +1,8 @@
 <?php
-echo "hello I'm ac"
+  $json_str = file_get_contents('php://input');  //接收request的body
+  $json_obj = json_decode($json_str);  //轉json格式
+  
+  $myfile = fopen("log.txt", "w+") or die("Unable to open file!");
+  fwrite($myfile, "\xEF\xBB\xBf".$json_str);  //在字串加入\xEF\xBB\xBf轉成utf8格式
+  fclose($myfile);
 ?>
