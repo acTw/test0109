4c14938659ab080ee31dc83a4392b23d20d32947
Andy Cheng <acheng@cht.com.tw>
2018-01-09 15:51:22 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -115,8 +115,7 @@
 			$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-				'Authorization: Bearer n4mZIQp9UqWXhCEgIg1fLmyjUeDMgCe/bF+4EOBDZ7fGscOgNGFsHTr3fGco/E7A5hq7A7jiDszSCk/j3pVVPbx7nf0E+FKe5jX6syQGOxO7kwp5lmZ3zRES1qxceq/N+/E9Qy5gSDbBx56l8sScTwdB04t89/1O/w1cDnyilFU=',
-			));
+				'Authorization: Bearer n4mXmKnNpbr0dT9O+ftyI/eN2PdRydTcAPAoPlR3O18KvE0GFx7anI74jmjYIXCBqLPhf1fnfnI6bx+e33U6Phs7o7IcSuIOFE46MDKTftN+yKaYtwugqbsKdPBBj53jWvj3bfPttPweP9DGNaW9PQdB04t89/1O/w1cDnyilFU=";			));
 				
 			$json_content = curl_exec($ch);
 			curl_close($ch);
